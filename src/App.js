2b908159412870e97ff65d95bfc7cee57f248539
Ahrar Hayat <ahrarhayat@gmail.com>
2022-03-21 22:48:55 +1100

    use effect and use state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App(props) {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+  
+  useEffect(() =>{console.log(`Its ${emotion} around here`)},[emotion]);
+  useEffect(() =>{console.log(`Its ${secondary} around here`)},[secondary]);
   return(
     <>
-    <h1>Current emotion is {emotion}.</h1>
+    <h1>Current emotion is {emotion} and {secondary}.</h1>
     <button onClick={() => setEmotion("happy")}>Happy</button>
+    <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
     <button onClick={() => setEmotion("fraustrated")}>Fraustrate</button>
     <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
     </>
