c6548de833d7b3f7462390d12fe0b1e8a69808aa
Ahrar Hayat <ahrarhayat@gmail.com>
2022-03-21 17:36:40 +1100

    properties added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 
+function SecretComponent()
+{
+  return (<div><h1>Secret information for authorised users only</h1></div>)
+}
 
-function App() {
-  return (
-    <div className="App">
-      More Components here eventually
-    </div>
-  );
+function RegularComponent()
+{
+  return (<div><h1>Everyone can see this component</h1></div>)
+}
+
+function App(props) {
+  return(
+    <>
+      {props.authorized ? <SecretComponent/> : <RegularComponent/> }
+      </>
+      );
+  //alternate method
+  // if(props.authorized)
+  // {
+  //   return(<SecretComponent/>)
+  // }
+  // else{
+  //   return(<RegularComponent/>)
+  // }
 }
 
 export default App;
